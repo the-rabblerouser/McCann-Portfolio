0626ace7158b7a9330c3d6d7b105a898f4da4437
Joe McCann <joemccann@Joes-MBP.home>
2021-12-06 11:27:32 -0500

    Create outside links

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,11 +33,33 @@ const Paragraph = styled.p`
 	color: #fff;
 	font-size: 1rem;
 	text-align: center;
-	width: 100%;
+	width: 90%;
 	margin: 1rem 0 0 0;
 
 	@media (min-width: 811px) {
 		font-size: 4rem;
+	}
+`;
+
+const LinksContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-evenly;
+	text-align: center;
+	margin-top: 3.5rem;
+	width: 22rem;
+
+	@media (min-width: 811px) {
+	}
+`;
+
+const MediaLink = styled.a`
+	color: #fff;
+	font-size: 0.8rem;
+
+	@media (min-width: 811px) {
+		font-size: 2rem;
 	}
 `;
 
@@ -58,6 +80,15 @@ const Home: NextPage = () => {
 					<Paragraph>
 						I am currently living in NYC and looking for work.
 					</Paragraph>
+					<LinksContainer>
+						<MediaLink href='mailto:tojmccann@gmail.com'>Email</MediaLink>
+						<MediaLink href='https://github.com/the-rabblerouser'>
+							Github
+						</MediaLink>
+						<MediaLink href='https://www.linkedin.com/in/joseph-mccann-77402a88/'>
+							LinkedIn
+						</MediaLink>
+					</LinksContainer>
 				</AboutContainer>
 			</AppContainer>
 		</>
