30147c2fb2d28af3eedb6bf4f4e961c58f55a34a
Joe McCann <joemccann@Joes-MBP.home>
2021-12-06 16:18:01 -0500

    Connect and Footer component created

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,9 +24,64 @@ const Container = styled.div`
 	align-items: center;
 	height: 20vh;
 	width: 100vw;
+	margin-top: 4rem;
 
 	@media (min-width: 811px) {
-		height: 50vh;
+		height: 23vh;
+		margin-top: 6rem;
+	}
+`;
+
+const Connect = styled.p`
+	color: #fff;
+	font-size: 0.8rem;
+
+	@media (min-width: 811px) {
+		font-size: 1.2rem;
+	}
+`;
+
+const Email = styled.a`
+	color: #fff;
+	font-size: 1rem;
+	margin-top: 1rem;
+	text-decoration: underline;
+
+	@media (min-width: 811px) {
+		font-size: 1rem;
+	}
+`;
+
+const LinksContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-evenly;
+	width: 18rem;
+	text-align: center;
+	margin-top: 3.5rem;
+
+	@media (min-width: 811px) {
+		margin-top: 5rem;
+		width: 20rem;
+	}
+`;
+
+const MediaLink = styled.a`
+	color: #fff;
+	font-size: 0.8rem;
+
+	@media (min-width: 811px) {
+		font-size: 1rem;
+	}
+`;
+
+const Brand = styled.p`
+	color: #fff;
+	font-size: 0.7rem;
+
+	@media (min-width: 811px) {
+		font-size: 0.8rem;
 	}
 `;
 
@@ -39,7 +94,19 @@ const Home: NextPage = () => {
 				<Hero />
 				<AboutMe />
 				<Projects />
-				<Container></Container>
+				<Container>
+					<Connect>Feel free to reach out if you're looking to connect</Connect>
+					<Email href='mailto:tojmccann@gmail.com'>tojmccann@gmail.com</Email>
+					<LinksContainer>
+						<Brand>Joe McCann {new Date().getFullYear()}</Brand>
+						<MediaLink href='https://github.com/the-rabblerouser'>
+							Github
+						</MediaLink>
+						<MediaLink href='https://www.linkedin.com/in/joseph-mccann-77402a88/'>
+							LinkedIn
+						</MediaLink>
+					</LinksContainer>
+				</Container>
 			</AppContainer>
 		</>
 	);
