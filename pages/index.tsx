22e2517e250dec492e9e267c60acf5e9f25086cd
Joe McCann <joemccann@Joes-MBP.home>
2021-12-06 12:03:12 -0500

    Created Container for projects to be displayed

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,11 +17,11 @@ const AppContainer = styled.div`
 	}
 `;
 
-export const ProjectsContainer = styled.div`
+const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	height: 95vh;
+	height: 90vh;
 	width: 100vw;
 
 	@media (min-width: 811px) {
@@ -30,13 +30,34 @@ export const ProjectsContainer = styled.div`
 
 const Title = styled.p`
 	color: #fff;
-	font-size: 2rem;
+	font-size: 0.8rem;
 	text-align: center;
 	margin: 2rem 0 2rem 0;
 
 	@media (min-width: 811px) {
 		font-size: 4rem;
 		margin: 4rem 0 4rem 0;
+	}
+`;
+
+const ProjectsContainer = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-template-rows: 1fr 1fr 1fr;
+	height: 80vh;
+	width: 100vw;
+
+	@media (min-width: 811px) {
+	}
+`;
+
+const Project = styled.div`
+	height: 100%;
+	width: 100%;
+	background: #fff;
+	border: 1px solid black;
+
+	@media (min-width: 811px) {
 	}
 `;
 
@@ -48,9 +69,18 @@ const Home: NextPage = () => {
 				{/* <Navbar /> */}
 				<Hero />
 				<AboutMe />
-				<ProjectsContainer>
-					<Title>Personal Projects</Title>
-				</ProjectsContainer>
+				<Container>
+					<Title>Check out some of my personal projects</Title>
+					<ProjectsContainer>
+						<Project />
+						<Project />
+						<Project />
+						<Project />
+						<Project />
+						<Project />
+					</ProjectsContainer>
+				</Container>
+				<Container></Container>
 			</AppContainer>
 		</>
 	);
