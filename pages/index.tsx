3b5c033a579c405ec913eb671ff822567a54c085
Joe McCann <joemccann@Joes-MBP.home>
2021-12-06 11:03:40 -0500

    added about me paragraphs

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,16 +29,36 @@ const AboutContainer = styled.div`
 	}
 `;
 
+const Paragraph = styled.p`
+	color: #fff;
+	font-size: 1rem;
+	text-align: center;
+	width: 100%;
+	margin: 1rem 0 0 0;
+
+	@media (min-width: 811px) {
+		font-size: 4rem;
+	}
+`;
+
 const Home: NextPage = () => {
 	return (
 		<>
 			<Particles id='tsparticles' options={particleOptions} />
 			<AppContainer>
 				{/* <Navbar /> */}
-
 				<Hero />
-
-				<AboutContainer></AboutContainer>
+				<AboutContainer>
+					<Paragraph>
+						I am a fullstack web devloper who focus is on the frontend.
+					</Paragraph>
+					<Paragraph>
+						My preferred tools are React, Next.js, MongoDB and Node.js.
+					</Paragraph>
+					<Paragraph>
+						I am currently living in NYC and looking for work.
+					</Paragraph>
+				</AboutContainer>
 			</AppContainer>
 		</>
 	);
