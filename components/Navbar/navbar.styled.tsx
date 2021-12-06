872ecaa7d7ac8bdbcfbbe5ee650ad2e47a5e3a47
Joe McCann <joemccann@Joes-MBP.home>
2021-12-06 09:32:09 -0500

    Optimize for desktop & Center and evenly space nav items

--- components/Navbar/navbar.styled.tsx
+++ components/Navbar/navbar.styled.tsx
@@ -3,21 +3,24 @@ import styled from 'styled-components';
 export const OuterContainer = styled.div`
 	display: flex;
 	justify-content: center;
+	align-items: center;
+	height: 5rem;
 	width: 100vw;
 
 	@media (min-width: 811px) {
-		justify-content: center;
+		height: 10rem;
 	}
 `;
 
 export const InnerContainer = styled.div`
 	display: flex;
 	justify-content: space-evenly;
-	align-items: center;
-	height: 5rem;
+	width: 100vw;
 
 	@media (min-width: 811px) {
-		width: 20%;
+		display: flex;
+		justify-content: space-evenly;
+		width: 50vw;
 	}
 `;
 
@@ -26,9 +29,17 @@ export const Projects = styled.button`
 	border: none;
 	color: #fff;
 	font-size: 1.2rem;
+
+	@media (min-width: 811px) {
+		font-size: 2rem;
+	}
 `;
 
 export const Contact = styled.a`
 	color: #fff;
 	font-size: 1.2rem;
+
+	@media (min-width: 811px) {
+		font-size: 2rem;
+	}
 `;
