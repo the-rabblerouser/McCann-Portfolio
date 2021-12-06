b98ed3221c7749f42ef7e0323fc04fb0fa544c62
Joe McCann <joemccann@Joes-MBP.home>
2021-12-05 19:06:40 -0500

    created container for hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
 import type { NextPage } from 'next';
 
+import styled from 'styled-components';
+
 import Navbar from '../components/Navbar';
 
 const Home: NextPage = () => {
+	const OuterContainer = styled.div`
+		display: flex;
+		justify-content: end;
+		height: 95vh;
+
+		@media (min-width: 811px) {
+		}
+	`;
+
 	return (
 		<>
-			<Navbar />
+			<OuterContainer>
+				<Navbar />
+			</OuterContainer>
 		</>
 	);
 };
