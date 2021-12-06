cbd000a9b5625fdc38037be7bafc49b375fa26de
Joe McCann <joemccann@Joes-MBP.home>
2021-12-06 09:18:07 -0500

    Correct dynamically called styled-component issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,31 +6,31 @@ import Particles from 'react-tsparticles';
 import Navbar from '../components/Navbar';
 import { particleOptions } from '../utils/particleOptions';
 
+const AppContainer = styled.div`
+	position: absolute;
+	top: 0;
+	padding-top: 0 @media (min-width: 811px) {
+
+	}
+`;
+const OuterContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 95vh;
+
+	@media (min-width: 811px) {
+	}
+`;
+
+//  const Img = styled.img`
+// height: 4rem;
+// width: 4rem;
+// margin: 5px;
+// `;
+
 const Home: NextPage = () => {
-	const AppContainer = styled.div`
-		position: absolute;
-		top: 0;
-		padding-top: 0 @media (min-width: 811px) {
-
-		}
-	`;
-	const OuterContainer = styled.div`
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 95vh;
-
-		@media (min-width: 811px) {
-		}
-	`;
-
-	//  const Img = styled.img`
-	// height: 4rem;
-	// width: 4rem;
-	// margin: 5px;
-	// `;
-
 	return (
 		<>
 			<Particles id='tsparticles' options={particleOptions} />
