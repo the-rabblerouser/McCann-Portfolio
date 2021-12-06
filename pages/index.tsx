3159bbc285e9e83d25a1d2404a975ed2eee4210a
Joe McCann <joemccann@Joes-MBP.home>
2021-12-06 11:49:06 -0500

    Personal Projects Title created with spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,11 +21,22 @@ export const ProjectsContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	justify-content: center;
-	height: 90vh;
+	height: 95vh;
 	width: 100vw;
 
 	@media (min-width: 811px) {
+	}
+`;
+
+const Title = styled.p`
+	color: #fff;
+	font-size: 2rem;
+	text-align: center;
+	margin: 2rem 0 2rem 0;
+
+	@media (min-width: 811px) {
+		font-size: 4rem;
+		margin: 4rem 0 4rem 0;
 	}
 `;
 
@@ -37,7 +48,9 @@ const Home: NextPage = () => {
 				{/* <Navbar /> */}
 				<Hero />
 				<AboutMe />
-				<ProjectsContainer></ProjectsContainer>
+				<ProjectsContainer>
+					<Title>Personal Projects</Title>
+				</ProjectsContainer>
 			</AppContainer>
 		</>
 	);
