8dd39bf848985dc2bf1784f6c8c17fa3b8818bca
Joe McCann <joemccann@Joes-MBP.home>
2021-12-08 10:19:23 -0500

    Navbar now actives on hover and nav items move from left to right

--- components/Navbar/navbar.styled.ts
+++ components/Navbar/navbar.styled.ts
@@ -13,9 +13,11 @@ export const OuterContainer = styled.div`
 
 export const InnerContainer = styled.div`
 	display: flex;
+	flex-direction: row-reverse;
 	justify-content: space-evenly;
 	align-items: center;
 	width: 100%;
+	height: 100%;
 	@media (min-width: 811px) {
 	}
 `;
@@ -69,7 +71,7 @@ export const NavLinks = styled.div`
 	width: 60%;
 	height: 100%;
 	transition-duration: 1s;
-	transform: translateX(400px);
+	transform: translateX(-400px);
 
 	${InnerContainer}:hover & {
 		transform: translateX(0px);
